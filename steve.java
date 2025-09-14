ed84be41c31c4f3c8bf40b46a76af525edb0001b
Mboni_Dev <mboninkunda@gmail.com>
2025-09-14 20:51:45 +0000

    Initial commit

---
+++ steve.java
@@ -0,0 +1,11 @@
+public class Steve {
+    public static int square(int number) {
+        return number * number;
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        int result = square(num);
+        System.out.println("Square of " + num + " is " + result);
+    }
+}
