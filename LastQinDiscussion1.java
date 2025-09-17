dcff098c01d0ea5d06c50c1a75c4cd748e1d78b0
Mboni777 <mboninkunda@gmail.com>
2025-09-17 21:23:56 +0000

    Initial commit

---
+++ LastQinDiscussion1.java
@@ -0,0 +1,7 @@
+public class LastQinDiscussion1 {
+    public static void main(String[] args) {
+        int age = 10;
+        if (age > 18)
+            System.out.println("You can vote!");
+    }
+}
